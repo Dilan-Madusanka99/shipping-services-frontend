5bf8ece107ca3cdf81d3e4dc492fde334cbc13b9
Chamith Mendis <m.chamith@directfn.com>
2025-07-08 23:40:59 +0300

    vessel save issue fixed

--- src/app/vessels/vessel-registration/vessel-registration.component.ts
+++ src/app/vessels/vessel-registration/vessel-registration.component.ts
@@ -14,219 +14,242 @@ export interface PeriodicElement {
   flag: string;
 }
 
-const ELEMENT_DATA: any[] = [ 
-  {vesselName: 'Dacil', imoNo: 'souselas', vesselType: 'bulk', flag: 'portugal'},
-];
+const ELEMENT_DATA: any[] = [{ vesselName: 'Dacil', imoNo: 'souselas', vesselType: 'bulk', flag: 'portugal' }];
 
 @Component({
   selector: 'app-vessel-registration',
   standalone: false,
   templateUrl: './vessel-registration.component.html',
   styleUrl: './vessel-registration.component.scss'
-}) 
+})
 export class VesselRegistrationComponent implements OnInit {
+  vesselRegistrationForm: FormGroup;
 
-  vesselRegistrationForm : FormGroup;
+  displayedColumns: string[] = ['vesselName', 'imoNo', 'vesselType', 'flag', 'actions'];
+  dataSource: MatTableDataSource<any>;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  selected: String;
+  saveButtonLabel = 'Save';
+  mode = 'add';
+  selectedData;
+  isButtonDisabled = false;
+  selectedFile: File | null = null;
+  previewUrl!: SafeUrl | null;
+  isFileSelected = false;
 
-      displayedColumns: string[] = ['vesselName', 'imoNo', 'vesselType', 'flag', 'actions'];
-      dataSource: MatTableDataSource<any>;
-      @ViewChild(MatPaginator) paginator: MatPaginator;
-      @ViewChild(MatSort) sort: MatSort;
-      selected: String;
-      saveButtonLabel = 'Save';
-      mode = 'add';
-      selectedData;
-      isButtonDisabled = false;
-      selectedFile: File | null = null;
-      previewUrl!: SafeUrl | null;
-      isFileSelected = false;
+  constructor(
+    private fb: FormBuilder,
+    private vesselRegistrationService: VesselRegistrationService,
+    private messageService: MessageServiceService,
+    private sanitizer: DomSanitizer
+  ) {
+    this.vesselRegistrationForm = this.fb.group({
+      profileImage: new FormControl(''),
+      profileImageName: new FormControl(''),
+      profileImageType: new FormControl(''),
+      vesselName: new FormControl(''),
+      imoNo: new FormControl('', [Validators.required, Validators.pattern(/^[0-9]+$/)]),
+      vesselType: new FormControl('', [Validators.required]),
+      flag: new FormControl('', [Validators.pattern(/^[A-Za-z]+$/)]),
+      yob: new FormControl('', [Validators.pattern(/^\d{4}$/)]),
+      grt: new FormControl('', [Validators.pattern(/^[0-9]+$/)]),
+      bhp: new FormControl('', [Validators.required])
+    });
+  }
 
-      constructor(private fb: FormBuilder, private vesselRegistrationService: VesselRegistrationService, private messageService: MessageServiceService, private sanitizer: DomSanitizer) {
-            this.vesselRegistrationForm = this.fb.group({
-              profileImage: new FormControl(''),
-              profileImageName: new FormControl(''),
-              profileImageType: new FormControl(''),
-              vesselName: new FormControl(''),
-              imoNo: new FormControl('', [Validators.required, Validators.pattern(/^[0-9]+$/)]),
-              vesselType: new FormControl('', [Validators.required]),
-              flag: new FormControl('', [Validators.pattern(/^[A-Za-z]+$/)]),
-              yob: new FormControl('', [Validators.pattern(/^\d{4}$/)]),
-              grt: new FormControl('', [Validators.pattern(/^[0-9]+$/)]),
-              bhp: new FormControl('', [Validators.required])
-            });
+  ngOnInit(): void {
+    this.populateData();
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  public populateData(): void {
+    try {
+      this.vesselRegistrationService.getData().subscribe({
+        next: (dataList: any[]) => {
+          if (dataList.length <= 0) {
+            return;
           }
 
-      ngOnInit(): void{
-      this.populateData();
+          this.dataSource = new MatTableDataSource(dataList);
+          this.dataSource.paginator = this.paginator;
+          this.dataSource.sort = this.sort;
+        },
+        error: (error) => {
+          this.messageService.showError('Action Failed With Error ' + error);
+        }
+      });
+    } catch (error) {
+      this.messageService.showError('Action Failed With Error ' + error);
     }
-  
-    applyFilter(event: Event) {
-      const filterValue = (event.target as HTMLInputElement).value;
-      this.dataSource.filter = filterValue.trim().toLowerCase();
-  
-      if (this.dataSource.paginator) {
-        this.dataSource.paginator.firstPage();
-      }
+  }
+
+  public prepareVesselsData(): FormData {
+    const vesselRegistrationFormData = new FormData();
+    vesselRegistrationFormData.append(
+      'vesselRegistrationForm',
+      new Blob([JSON.stringify(this.vesselRegistrationForm.value)], {
+        type: 'application/json'
+      })
+    );
+
+    if (this.isFileSelected) {
+      vesselRegistrationFormData.append(
+        'profileImage',
+        this.vesselRegistrationForm.get('profileImage')?.value,
+        this.vesselRegistrationForm.get('profileImage')?.value.name
+      );
+    } else {
+      const imageBlob = this.base64ToBlob(
+        this.vesselRegistrationForm.get('profileImage')?.value,
+        this.vesselRegistrationForm.get('profileImageImageType')?.value
+      );
+      const file = new File([imageBlob], this.vesselRegistrationForm.get('profileImageImageName')?.value, {
+        type: this.vesselRegistrationForm.get('profileImageImageType')?.value
+      });
+      vesselRegistrationFormData.append('profileImage', file, file.name);
     }
-  
-    public populateData(): void {
-      try {
-        this.vesselRegistrationService.getData().subscribe({
-          next: (dataList: any[]) => {
-            if (dataList.length <=0) {
-              return;
+    return vesselRegistrationFormData;
+  }
+
+  base64ToBlob(base64: string, mimeType: string): Blob {
+    const byteCharacters = atob(base64);
+    const byteNumbers = new Array(byteCharacters.length);
+    for (let i = 0; i < byteCharacters.length; i++) {
+      byteNumbers[i] = byteCharacters.charCodeAt(i);
+    }
+    const byteArray = new Uint8Array(byteNumbers);
+    return new Blob([byteArray], { type: mimeType });
+  }
+
+  onFileSelected(event: any): void {
+    if (event.target.files) {
+      const file = event.target.files[0];
+      const url = this.sanitizer.bypassSecurityTrustUrl(window.URL.createObjectURL(file));
+      this.previewUrl = url;
+      this.isFileSelected = true;
+      this.vesselRegistrationForm.get('profileImage')?.setValue(file);
+    }
+  }
+
+  onSubmit() {
+    try {
+      console.log('mode' + this.mode);
+      console.log('Form Submitted');
+      console.log(this.vesselRegistrationForm.value);
+
+      if (this.mode === 'add') {
+        this.vesselRegistrationService.serviceCall(this.prepareFormData()).subscribe({
+          next: (response: any) => {
+            if (this.dataSource && this.dataSource.data && this.dataSource.data.length > 0) {
+              this.dataSource = new MatTableDataSource([response, ...this.dataSource.data]);
+            } else {
+              this.dataSource = new MatTableDataSource([response, ...this.dataSource.data]);
             }
-            
-            this.dataSource = new MatTableDataSource(dataList);
-            this.dataSource.paginator = this.paginator;
-            this.dataSource.sort = this.sort;
+
+            this.messageService.showSuccess('Data Saved Successfully!');
           },
           error: (error) => {
-            this.messageService.showError('Action Failed With Error ' + error);
+            this.messageService.showError('Action Failed With Error' + error);
           }
         });
-      } catch (error) {
-        this.messageService.showError('Action Failed With Error ' + error);
+      } else if (this.mode === 'edit') {
+        this.vesselRegistrationService.editData(this.selectedData?.id, this.prepareFormData()).subscribe({
+          next: (response: any) => {
+            let elementIndex = this.dataSource.data.findIndex((element) => element.id === this.selectedData?.id);
+            this.dataSource.data[elementIndex] = response;
+            this.dataSource = new MatTableDataSource(this.dataSource.data);
+            this.messageService.showSuccess('Data Edited Successfully!');
+          },
+          error: (error) => {
+            this.messageService.showError('Action Failed With Error' + error);
+          }
+        });
       }
+      this.mode = 'add';
+      this.vesselRegistrationForm.disable();
+      this.isButtonDisabled = true;
+    } catch (error) {
+      console.log(error);
+      this.messageService.showError('Action Failed With Error' + error);
     }
+  }
 
-    public prepareVesselsData(): FormData {
-      const vesselRegistrationFormData = new FormData();
-      vesselRegistrationFormData.append(
-        'vesselRegistrationForm',
-        new Blob([JSON.stringify(this.vesselRegistrationForm.value)], {
-          type: 'application/json',
-        })
+  public resetData(): void {
+    this.vesselRegistrationForm.reset();
+    this.saveButtonLabel = 'Save';
+    this.vesselRegistrationForm.enable();
+    this.isButtonDisabled = false;
+  }
+
+  public editData(data: any): void {
+    this.vesselRegistrationForm.patchValue(data);
+    this.saveButtonLabel = 'Edit';
+    this.mode = 'edit';
+    this.selectedData = data;
+  }
+
+  public deleteData(data: any): void {
+    const id = data.id;
+
+    try {
+      this.vesselRegistrationService.deleteData(id).subscribe({
+        next: (response: any) => {
+          const index = this.dataSource.data.findIndex((element) => element.id === id);
+
+          if (index !== -1) {
+            this.dataSource.data.splice(index, 1);
+          }
+          this.dataSource = new MatTableDataSource(this.dataSource.data);
+          this.messageService.showSuccess('Data Deleted Successfully!');
+        },
+        error: (error: any) => {
+          this.messageService.showError('Action Failed With Error' + error);
+        }
+      });
+    } catch (error) {
+      console.log(error);
+      this.messageService.showError('Action Failed With Error' + error);
+    }
+  }
+
+  public refreshData(): void {
+    this.populateData();
+  }
+
+  public prepareFormData(): FormData {
+    const vesselFormData = new FormData();
+    vesselFormData.append(
+      'vesselRegistrationForm',
+      new Blob([JSON.stringify(this.vesselRegistrationForm.value)], {
+        type: 'application/json'
+      })
+    );
+
+    if (this.isFileSelected) {
+      vesselFormData.append(
+        'profileImage',
+        this.vesselRegistrationForm.get('profileImage')?.value,
+        this.vesselRegistrationForm.get('profileImage')?.value.name
       );
-  
-      if (this.isFileSelected) {
-        vesselRegistrationFormData.append(
-          'profileImage',
-          this.vesselRegistrationForm.get('profileImage')?.value,
-          this.vesselRegistrationForm.get('profileImage')?.value.name
-        );
-      } else {
-        const imageBlob = this.base64ToBlob(
-          this.vesselRegistrationForm.get('profileImage')?.value,
-          this.vesselRegistrationForm.get('profileImageImageType')?.value
-        );
-        const file = new File(
-          [imageBlob],
-          this.vesselRegistrationForm.get('profileImageImageName')?.value,
-          { type: this.vesselRegistrationForm.get('profileImageImageType')?.value }
-        );
-        vesselRegistrationFormData.append('profileImage', file, file.name);
-      }
-      return vesselRegistrationFormData;
+    } else {
+      const imageBlob = this.base64ToBlob(
+        this.vesselRegistrationForm.get('profileImage')?.value,
+        this.vesselRegistrationForm.get('profileImageImageType')?.value
+      );
+      const file = new File([imageBlob], this.vesselRegistrationForm.get('profileImageImageName')?.value, {
+        type: this.vesselRegistrationForm.get('profileImageImageType')?.value
+      });
+      vesselFormData.append('profileImage', file, file.name);
     }
-
-    base64ToBlob(base64: string, mimeType: string): Blob {
-      const byteCharacters = atob(base64);
-      const byteNumbers = new Array(byteCharacters.length);
-      for (let i = 0; i < byteCharacters.length; i++) {
-        byteNumbers[i] = byteCharacters.charCodeAt(i);
-      }
-      const byteArray = new Uint8Array(byteNumbers);
-      return new Blob([byteArray], { type: mimeType });
-    }
-
-    onFileSelected(event: any): void {
-
-      if (event.target.files) {
-        const file = event.target.files[0];
-        const url = this.sanitizer.bypassSecurityTrustUrl(
-          window.URL.createObjectURL(file)
-        );
-        this.previewUrl = url;
-        this.isFileSelected = true;
-        this.vesselRegistrationForm.get('profileImage')?.setValue(file);
-      }
-    }
-  
-    onSubmit() {
-        try {
-          console.log('mode' + this.mode);
-          console.log('Form Submitted');
-          console.log(this.vesselRegistrationForm.value);
-  
-          if (this.mode === 'add'){
-            this.vesselRegistrationService.serviceCall(this.vesselRegistrationForm.value).subscribe({
-              next: (response: any) => {
-                if (this.dataSource && this.dataSource.data && this.dataSource.data.length > 0) {
-                  this.dataSource = new MatTableDataSource([response, ...this.dataSource.data]);
-                } else {
-                    this.dataSource = new MatTableDataSource([response, ...this.dataSource.data]);
-                }        
-        
-                this.messageService.showSuccess('Data Saved Successfully!');  
-              },
-              error: (error) => {
-                this.messageService.showError('Action Failed With Error' + error);
-              }
-            });
-          }
-          else if (this.mode === 'edit'){
-            this.vesselRegistrationService.editData(this.selectedData?.id, this.vesselRegistrationForm.value).subscribe ({
-              next: (response: any) => {
-                let elementIndex = this.dataSource.data.findIndex((element) => element.id === this.selectedData?.id);
-                this.dataSource.data[elementIndex] = response;
-                this.dataSource = new MatTableDataSource(this.dataSource.data);
-                this.messageService.showSuccess('Data Edited Successfully!');
-              },
-              error: (error) => {
-                this.messageService.showError('Action Failed With Error' + error);
-              }
-            });
-          }
-          this.mode = 'add';
-          this.vesselRegistrationForm.disable();
-          this.isButtonDisabled = true;
-        } catch (error) {
-          console.log(error);
-          this.messageService.showError('Action Failed With Error' + error);
-        }
-      }
-  
-      public resetData(): void {
-        this.vesselRegistrationForm.reset();
-        this.saveButtonLabel = 'Save';
-        this.vesselRegistrationForm.enable();
-        this.isButtonDisabled = false;
-      }
-  
-      public editData(data: any): void {
-        this.vesselRegistrationForm.patchValue(data);
-        this.saveButtonLabel = 'Edit';
-        this.mode = 'edit';
-        this.selectedData = data;
-      }
-  
-      public deleteData(data: any): void {
-        const id = data.id;
-        
-        try {
-          this.vesselRegistrationService.deleteData(id).subscribe ({
-            next: (response: any) => {
-              const index = this.dataSource.data.findIndex((element) => element.id === id);
-    
-            if (index !== -1) {
-              this.dataSource.data.splice(index, 1);
-            }
-            this.dataSource = new MatTableDataSource (this.dataSource.data);
-            this.messageService.showSuccess('Data Deleted Successfully!');
-            },
-            error: (error: any) => {
-              this.messageService.showError('Action Failed With Error' + error);
-            }
-          });
-        } catch (error) {
-          console.log(error);
-          this.messageService.showError('Action Failed With Error' + error);
-        }
-      }
-  
-      public refreshData(): void {
-        this.populateData();
-      } 
-
+    return vesselFormData;
+  }
 }
