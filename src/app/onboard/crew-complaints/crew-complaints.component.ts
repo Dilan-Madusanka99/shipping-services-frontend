f110edb2dafc45205e9bb8c8d6eff9170e7f967d
Dilan Madusanka <dilanmadushanka800@gmail.com>
2026-02-05 16:58:34 +0530

    Update crew-complaints.component.ts

--- src/app/onboard/crew-complaints/crew-complaints.component.ts
+++ src/app/onboard/crew-complaints/crew-complaints.component.ts
@@ -121,22 +121,61 @@ export class CrewComplaintsComponent {
     }
   }
 
-  public populateData(): void {
-    try {
-      this.crewComplaintsService.getData().subscribe({
-        next: (dataList: any[]) => {
-          if (dataList.length <= 0) {
-            return;
-          }
+  // public populateData(): void {
+  //   try {
+  //     this.crewComplaintsService.getData().subscribe({
+  //       next: (dataList: any[]) => {
+  //         if (dataList.length <= 0) {
+  //           return;
+  //         }
 
-          this.dataSource = new MatTableDataSource(dataList);
-          this.dataSource.paginator = this.paginator;
-          this.dataSource.sort = this.sort;
-        },
-        error: (error) => {
-          this.messageService.showError('Action Failed With Error ' + error);
-        }
-      });
+  //         this.dataSource = new MatTableDataSource(dataList);
+  //         this.dataSource.paginator = this.paginator;
+  //         this.dataSource.sort = this.sort;
+  //       },
+  //       error: (error) => {
+  //         this.messageService.showError('Action Failed With Error ' + error);
+  //       }
+  //     });
+  //   } catch (error) {
+  //     this.messageService.showError('Action Failed With Error ' + error);
+  //   }
+  // }
+
+   public populateData(): void {
+    try {
+      if (window.localStorage.getItem('role') === 'SEAFARER') {
+        /* If the role is seafarer then get only details related to SID no*/ 
+        this.seafarersService.getSeafarerData(window.localStorage.getItem('sid')).subscribe({
+          next: (data) => {
+            if (!data) {
+              return;
+            }
+
+            this.dataSource = new MatTableDataSource([data]);
+            this.dataSource.paginator = this.paginator;
+            this.dataSource.sort = this.sort;
+          },
+          error: (error) => {
+            this.messageService.showError('Action Failed With Error ' + error);
+          }
+        });
+      } else {
+        this.seafarersService.getData().subscribe({
+          next: (dataList: any[]) => {
+            if (dataList.length <= 0) {
+              return;
+            }
+
+            this.dataSource = new MatTableDataSource(dataList);
+            this.dataSource.paginator = this.paginator;
+            this.dataSource.sort = this.sort;
+          },
+          error: (error) => {
+            this.messageService.showError('Action Failed With Error ' + error);
+          }
+        });
+      }
     } catch (error) {
       this.messageService.showError('Action Failed With Error ' + error);
     }
